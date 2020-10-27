454e4e1dc3f0c79e5ccee69389c6d07ea5421294
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2020-10-27 13:58:50 +0300

    Updates

---
+++ precache-manifest.765898fcdf0608c8bb9730b3709c65ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0418ec229adc6667bb2db0fd1da43366",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "507b1640790c0c1f6176",
+    "url": "/robotfriends/static/css/main.78b60e0f.chunk.css"
+  },
+  {
+    "revision": "0b72697a3c66042620d7",
+    "url": "/robotfriends/static/js/2.445e9eaa.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/robotfriends/static/js/2.445e9eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507b1640790c0c1f6176",
+    "url": "/robotfriends/static/js/main.81d09a42.chunk.js"
+  },
+  {
+    "revision": "967e2edc7d854880245a",
+    "url": "/robotfriends/static/js/runtime-main.0853c300.js"
+  }
+]);
